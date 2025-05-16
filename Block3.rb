6c76f167a4349ba7ba8978013cf904c6b124e1dc
Romylia <43366427+Romylia@users.noreply.github.com>
2025-05-16 12:55:44 +0300

    Update Block3.rb
    Still WIP

--- Block3.rb
+++ Block3.rb
@@ -7,16 +7,14 @@ class DiscreteVariationSeries
         raise "Недопустимый тип инициализации!"
       end
 
-      for el in values
+      if (initialization_type == "probability" or initialization_type = "p" or initialization_type == 1) then
+        summ = 0
+        
+        for el in values
           if not(el[0].is?(Numeric)) then 
             raise "Недопустимое значение случайной величины!"
           end
-      end
-        
-      if (initialization_type == "probability" or initialization_type = "p" or initialization_type == 1) then
-        summ = 0
-      
-        for el in values
+
           if not(el[1].is?(Numeric)) or el[1] < 0 or el[1] > 1 then 
             raise "Недопустимое значение вероятности!"
           end
@@ -31,6 +29,10 @@ class DiscreteVariationSeries
         probability_initializer(values)
       else
         for el in values
+          if not(el[0].is?(Numeric)) then 
+            raise "Недопустимое значение случайной величины!"
+          end
+          
           if not(el[1].is?(Integer)) or el[1] < 0 then 
             raise "Недопустимое значение частоты!"
           end
@@ -79,13 +81,14 @@ class DiscreteVariationSeries
     end
     summ
   end
-  
   private
 
   def probability_initializer(values)
     @series = Array.new
+    summ = 0
 
     for el in values
+      summ += el[1]
       @series += [[el[0], el[1], 0]]
     end
 
